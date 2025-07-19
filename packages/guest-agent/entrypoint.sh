7b538f987e180c8077a43f994653df44e9387cb9
Carsten Munk <carsten@zippie.com>
2025-07-19 13:13:04 +0200

    RUST_LOG=debug of the guest agent
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- packages/guest-agent/entrypoint.sh
+++ packages/guest-agent/entrypoint.sh
@@ -5,7 +5,7 @@ socat VSOCK-LISTEN:8080,fork TCP:127.0.0.1:8080 &
 socat VSOCK-LISTEN:8022,fork TCP:127.0.0.1:22 &
 echo "CMIO guest agent setting: $CMIO_GUEST_AGENT" >> /dev/console
 if [ x$CMIO_GUEST_AGENT != x ]; then
-        /bin/guest-agent >> /dev/console
+        RUST_LOG=debug /bin/guest-agent >> /dev/console
 fi
 echo "Guest agent failed to start" >> /dev/console
 while true; do
