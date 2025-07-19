68935a0e8964b3cc7de9fc540fc86e58a46e4a2e
Carsten Munk <carsten@zippie.com>
2025-07-19 15:58:42 +0200

    what is going on here
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- packages/guest-agent/entrypoint.sh
+++ packages/guest-agent/entrypoint.sh
@@ -5,7 +5,8 @@ socat VSOCK-LISTEN:8080,fork TCP:127.0.0.1:8080 &
 socat VSOCK-LISTEN:8022,fork TCP:127.0.0.1:22 &
 echo "CMIO guest agent setting: $CMIO_GUEST_AGENT" >> /dev/console
 if [ x$CMIO_GUEST_AGENT != x ]; then
-        RUST_LOG=debug /bin/guest-agent 2>&1 >> /dev/console
+		echo "Starting guest agent" >> /dev/console
+        RUST_LOG=trace /bin/guest-agent >> /dev/console 2>&1
 fi
 echo "Guest agent failed to start" >> /dev/console
 while true; do
