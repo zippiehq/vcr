065435f9264bee7743f2513c42d988ce874567eb
Carsten Munk <carsten@zippie.com>
2025-07-19 12:58:46 +0200

    feat: guest agent, start CMIO_GUEST_AGENT if set as env
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- packages/guest-agent/entrypoint.sh
+++ packages/guest-agent/entrypoint.sh
@@ -3,8 +3,11 @@ ip link set lo up
 ip addr
 socat VSOCK-LISTEN:8080,fork TCP:127.0.0.1:8080 &
 socat VSOCK-LISTEN:8022,fork TCP:127.0.0.1:22 &
-#/bin/guest-agent
+echo "CMIO guest agent setting: $CMIO_GUEST_AGENT" >> /dev/console
+if [ x$CMIO_GUEST_AGENT != x ]; then
+        /bin/guest-agent >> /dev/console
+fi
 echo "Guest agent failed to start" >> /dev/console
 while true; do
-	sleep 99999
+        sleep 99999
 done
